9180832c60ef47964252243c2730d51b4fffeca4
kirafadhil <95693500n@gmail.com>
2020-09-22 15:03:57 +0700

    membuat migrate database

---
+++ app/Controllers/Mahasiswa.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\MahasiswaModel;
+
+class Mahasiswa extends BaseController
+{
+  public function index()
+  {
+    echo "Hello World";
+  }
+}
---
+++ app/Database/Migrations/2020-09-22-074755_mahasiswa.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Mahasiswa extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+			'id_mhs' => [
+				'type' => 'INT',
+				'constraint' => 11,
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			],
+			'nm_mhs' => [
+				'type' => 'VARCHAR',
+				'constraint' => 100,
+			],
+			'nim_mhs' => [
+				'type' => 'INT',
+				'constraint' => 11,
+			],
+			'jurusan' => [
+				'type' => 'VARCHAR',
+				'constraint' => 100,
+			],
+		]);
+		$this->forge->addKey('id_mhs', TRUE);
+		$this->forge->createTable('mahasiswa');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		//
+	}
+}
